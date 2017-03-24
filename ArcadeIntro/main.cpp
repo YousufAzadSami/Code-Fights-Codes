b9d921196614f8b1ff331f4368ca0cb49edd453c
sami <sami1592@gmail.com>
2017-03-24 19:54:04 +0600

    Intro > Level 1

--- ArcadeIntro/main.cpp
+++ ArcadeIntro/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+// Intro > Level 1
+bool checkPalindrome(std::string inputString)
+{
+    int _length = inputString.length();
+
+    for(int _first = 0, _last = _length-1; _first <= _last; _first++, _last--)
+    {
+        // debug code
+        std::cout << "First : " << inputString.at(_first) << " : Last: " << inputString.at(_last) << std::endl;
+
+        if(inputString.at(_first) != inputString.at(_last))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main()
 {
     std::cout << "Hello world!" << std::endl;
